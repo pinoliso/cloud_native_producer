0e8ba6cc38df5d51801576c2f448afcbfaeb360f
Enrique Pino <pinoliso@gmail.com>
2025-01-25 13:07:27 -0300

    last update

--- src/main/java/com/duoc/config/RabbitMQConfig.java
+++ src/main/java/com/duoc/config/RabbitMQConfig.java
@@ -7,6 +7,6 @@ public class RabbitMQConfig {
 
     public static final String EXCHANGE_NAME = "cloud_native_exchange";
     public static final String QUEUE_NAME = "cloud_native_queue";
-    public static final String ROUTING_KEY = "cloud_native_routing_key";
+    public static final String ROUTING_KEY = "cloud_native";
 
 }
